184890db32ba9642943e8c0e9296758251d10035
gtyene <gtyene@student.21-school.ru>
2021-11-21 14:52:40 +0300

    for merge

--- sources/builtins/minishell.c
+++ sources/builtins/minishell.c
@@ -235,6 +235,7 @@ int	main(int argc, char **argv, char **env)
 	init_start_struct(&env_ms, env);
 	pr = (t_parser *)malloc(sizeof(t_parser));
 	/*	Loop reading and executing lines until the use quit. */ 
+	
 	while (1)
 	{
 		pr->env = list_to_array(env_ms);
