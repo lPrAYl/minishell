117169adb3f3823a34e02ed00ae9933320a11848
salyce <salyce@student.21-school.ru>
2021-11-23 20:58:42 +0300

    norme parsed_red

---
+++ parsing/parser_red2.c
@@ -0,0 +1,26 @@
+#include "../includes/minishell.h"
+
+int	ft_parser_red_out12(t_token *new, int i)
+{
+	if (new->cmd[i][0] == '>' && new->cmd[i][1] != '>')
+	{
+		ft_close_fd(new, 1);
+		new->fd1 = open(new->cmd[i + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		if (new->fd1 < 0)
+		{
+			new->error = break_on_error(new->cmd[i + 1], strerror(errno), 1);
+			return (1);
+		}
+		ft_free_str_in_token(new, i + 1);
+		ft_free_str_in_token(new, i);
+		new->redorpipe = 21;
+	}
+	if (new->cmd[i][0] == '>' && new->cmd[i][1] == '>')
+	{
+		if (ft_append_red(new, &i) == 1)
+			return (1);
+		ft_free_str_in_token(new, i);
+		new->redorpipe = 22;
+	}
+	return (0);
+}
